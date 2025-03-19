365ca96a707983a6055e60684bd781b70b05b609
George Curry <Georgemcurry1@gmail.com>
2025-03-19 17:03:37 -0500

    Added new main file

---
+++ Conversion.py
@@ -0,0 +1,20 @@
+import tkinter
+#will try a GUI interface this time
+#this is psudo code
+
+def convert1(n):
+    #fahrenheit to celsius
+    #n is the input
+    print("do conversion rate")
+    return #converted number return here
+
+def convert2(n):
+    #celsius to fahrenheit
+    #n is the input
+    print("do conversion rate")
+    return #converted number return here
+
+#testing python gui, this opens up a blank window titled Conversion
+root = tkinter.Tk(screenName=None, baseName=None, className='Conversion', useTk=1)
+
+root.mainloop()
